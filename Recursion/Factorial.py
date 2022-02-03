92687ed8412b6049082d8840ffcd059cdc3d203c
Kundan Singh Chandel <devwork.ksc@gmail.com>
2022-02-03 15:52:12 +0530

    Create Factorial.py

---
+++ Recursion/Factorial.py
@@ -0,0 +1,15 @@
+def factorial(n):
+  return 1 if n<=1 else n*factorial(n-1)
+
+print(factorial(10))
+
+
+# -----------------------------------
+
+
+# def factorial(n):
+#     if(n<=1):
+#         return 1
+#     return n*factorial(n-1)
+
+# print(factorial(10))
